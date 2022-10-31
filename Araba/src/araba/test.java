d2829c4cce3454f848b14c432b6d9d94b38cb10a
syara <syara@DESKTOP-IDIU4TT>
2022-10-31 09:12:34 +0300

    Araba deneme

---
+++ Araba/src/araba/test.java
@@ -0,0 +1,18 @@
+package araba;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		 Araba araba1 = new Araba();
+		 
+		 araba1.setModel("Renault");
+		 
+		 System.out.println("The model of the car is: "+ araba1.getModel());
+		 		
+
+	}
+
+}
